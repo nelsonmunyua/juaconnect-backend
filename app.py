55a23deeba125fda1ace2ff0bca815407a5b4ca0
Nelson Munyua <nelsonmunyua8@gmail.com>
2026-01-13 17:03:38 +0300

    Updated relationships in the database and published it

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_migrate import Migrate
+from models import db
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///juaconnet.db"
+
+app.config["SQLALCHEMY_ECHO"] = True
+
+migrate = Migrate(app, db)
+
+# link db to the flask instance
+db.init_app(app)
+
+# db.init_app(app)
+@app.route('/')
+def index():
+    return {'message' : 'Welcome to ags backend'}, 200
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
